4d51ed140ebba6e22924c077573ecaa28fbaddb9
luca-cucchi81 <lu.cucchi@gmail.com>
2021-03-07 17:26:38 +0100

    working on drawer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import { Row, Col, Button, Drawer } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebookSquare,
+    faLinkedin,
+    faGithubSquare,
+} from "@fortawesome/free-brands-svg-icons";
 import AOS from "aos";
 import Jumbotron from "./components/jumbotron/Jumbotron";
 import Skills from "./components/skills/Skills";
@@ -10,6 +17,7 @@ import Portfolio from "./components/portfolio/Portfolio";
 import "antd/dist/antd.css";
 import "aos/dist/aos.css";
 import "./App.css";
+import avatar from "./assets/avatar.png";
 
 function App() {
     useEffect(() => {
@@ -28,61 +36,131 @@ function App() {
         setVisible(!visible);
     };
 
+    /* social icons */
+    const facebook = <FontAwesomeIcon icon={faFacebookSquare} />;
+    const linkedin = <FontAwesomeIcon icon={faLinkedin} />;
+    const github = <FontAwesomeIcon icon={faGithubSquare} />;
+
     return (
-        <div className="app">
-            <Button
-                ghost
-                type="link"
-                size="large"
-                icon={<MenuOutlined />}
-                onClick={showDrawer}
-                style={{
-                    position: "absolute",
-                    top: "10px",
-                    left: "10px",
-                    color: "#fff",
-                }}
-            ></Button>
-            <Drawer
-                placement="left"
-                closable={false}
-                onClose={closeDrawer}
-                visible={visible}
-                bodyStyle={{
-                    background: "linear-gradient(to bottom, #71b280, #134e5e)",
-                }}
-            >
-                <p>Portfolio</p>
-                <p>About Me</p>
-            </Drawer>
-            <Row className="jumbotron-row">
-                <Col
-                    style={{ display: "flex", alignItems: "center" }}
-                    span={12}
-                    offset={6}
+        <Router>
+            <div className="app">
+                <Button
+                    ghost
+                    type="link"
+                    size="large"
+                    icon={<MenuOutlined />}
+                    onClick={showDrawer}
+                    style={{
+                        position: "absolute",
+                        top: "10px",
+                        left: "10px",
+                        color: "#fff",
+                    }}
+                ></Button>
+                <Drawer
+                    placement="left"
+                    closable={false}
+                    onClose={closeDrawer}
+                    visible={visible}
+                    bodyStyle={{
+                        background: "#004953",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
                 >
-                    <Jumbotron />
-                </Col>
-            </Row>
-            <Row>
-                <Col
-                    style={{ display: "flex", alignItems: "center" }}
-                    span={12}
-                    offset={6}
-                >
-                    <Skills />
-                </Col>
-            </Row>
-            <Row>
-                <Col
-                    style={{ display: "flex", alignItems: "center" }}
-                    span={12}
-                    offset={6}
-                >
-                    <Portfolio />
-                </Col>
-            </Row>
-        </div>
+                    <div className="topDrawer">
+                        <img className="avatar" src={avatar} alt="avatar" />
+                        <div className="links">
+                            <ul>
+                                <li>
+                                    <NavLink to="/" className="nav_link">
+                                        Home
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/portfolio"
+                                        className="nav_link"
+                                    >
+                                        Portfolio
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink
+                                        to="/about-me"
+                                        className="nav_link"
+                                    >
+                                        About Me
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="social">
+                        <h2>Social</h2>
+                        <section className="social-icons">
+                            <a
+                                href="https://www.facebook.com/luca.cucchi"
+                                style={{
+                                    color: "#16c79a",
+                                    fontSize: 40,
+                                }}
+                            >
+                                {facebook}
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/luca-cucchi-088b5562/"
+                                style={{
+                                    color: "#16c79a",
+                                    fontSize: 40,
+                                }}
+                            >
+                                {linkedin}
+                            </a>
+                            <a
+                                href="https://github.com/luca-cucchi81"
+                                style={{
+                                    color: "#16c79a",
+                                    fontSize: 40,
+                                }}
+                            >
+                                {github}
+                            </a>
+                        </section>
+                    </div>
+                </Drawer>
+                <Row className="jumbotron-row">
+                    <Col
+                        style={{ display: "flex", alignItems: "center" }}
+                        span={12}
+                        offset={6}
+                    >
+                        <Jumbotron />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col
+                        style={{ display: "flex", alignItems: "center" }}
+                        span={12}
+                        offset={6}
+                    >
+                        <Skills />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col
+                        style={{ display: "flex", alignItems: "center" }}
+                        span={12}
+                        offset={6}
+                    >
+                        <Portfolio />
+                    </Col>
+                </Row>
+            </div>
+        </Router>
     );
 }
 
